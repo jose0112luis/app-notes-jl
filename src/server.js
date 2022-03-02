9eb0e472a42e82b495596b2a257c81eefca08bd1
Jose0112Luis <jose0112luis@hotmail.com>
2022-03-02 16:30:56 -0500

    Mejorando sistema de rutas con express y agregando estilos a index-about

--- src/server.js
+++ src/server.js
@@ -23,9 +23,7 @@ app.use(express.urlencoded({extended: false})); //convertir los datos a JSON
 // Global Variables
 
 // Routes
-app.get('/', (req,res) => {
-    res.render('index');
-});
+app.use(require('./routes/index.routes'));
 
 // Static Files
 app.use(express.static(path.join(__dirname, 'public')));

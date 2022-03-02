34838530fcdb46a73945defce57451ea54b7fee7
Jose0112Luis <jose0112luis@hotmail.com>
2022-03-01 23:05:28 -0500

    Agregando motor de plantillas handlebars

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 // --codio de express
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 // Initializations
@@ -8,6 +9,13 @@ const app = express();
 // Settings
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views')); //ruta completa de carpeta views desde cualquier SO
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 // Middlewares
 app.use(express.urlencoded({extended: false})); //convertir los datos a JSON
@@ -16,7 +24,7 @@ app.use(express.urlencoded({extended: false})); //convertir los datos a JSON
 
 // Routes
 app.get('/', (req,res) => {
-    res.send('Hello World');
+    res.render('index');
 });
 
 // Static Files
